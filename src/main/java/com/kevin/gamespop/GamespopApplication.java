5d186a967134b1c32293b906b85788a5f8d379fd
Kevin Alexsander De Jesus1 <kjesus1@Lenovo.com>
2022-06-30 14:22:03 -0300

    starters added all starters to the project

---
+++ src/main/java/com/kevin/gamespop/GamespopApplication.java
@@ -0,0 +1,13 @@
+package com.kevin.gamespop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamespopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamespopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kevin/gamespop/GamespopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.gamespop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamespopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
