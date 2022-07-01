29762b6c72d7b5f0b0b195d1d656886397e7dd94
Kevin Alexsander De Jesus1 <kjesus1@Lenovo.com>
2022-07-01 11:17:09 -0300

    added a DTO folder

---
+++ gamespop/src/main/java/com/kevin/gamespop/DTO/GameDTO.java
@@ -0,0 +1,12 @@
+package com.kevin.gamespop.DTO;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+//@AllArgsConstructor
+public class GameDTO {
+}
